bb37b980fe9f38702c0de3d7eefd98ec2b9add83
DEMBA DEMBELE <dembadembele@DEMBAs-MacBook-Pro.local>
2024-01-11 23:20:25 +0000

    Adding environment variables to init.py

--- board/__init__.py
+++ board/__init__.py
@@ -1,11 +1,21 @@
+import os
+# import load_dotenv
+from dotenv import load_dotenv
 
 from flask import Flask
 
-from board import pages
+from board import pages, posts
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_prefixed_env()
 
     app.register_blueprint(pages.bp)
+    app.register_blueprint(posts.bp)
+
+    print(f"Current Environment: {os.getenv('ENVIRONMENT')}")
+    print(f"Using Database: {app.config.get('DATABASE')}")
 
     return app
